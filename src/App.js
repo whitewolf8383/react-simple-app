7f5c2a03ad5f82dbfb93fb5473a1f90db6421b67
whitewolf8383 <kandt.vandf@gmail.com>
2021-10-01 23:17:52 -0600

    Restart

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Components
+import Counter from './components/counter';
+
+class App extends React.Component {
+  render() { 
+    return (
+      <div>
+        <Counter />
+      </div>
+    );
+  }
+}
+ 
+export default App;
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,47 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 class Counter extends React.Component {
-  styles = {
-    button: {
-      fontWeight: 30,
-      fontWeight: 'bold',
-      marginLeft: 5,
-      padding: 5,
-      width: 50
-    }
-  }
-  
   render() { 
-    return (
-      <div>
-        <span className={this.getBadgeClasses()}>{ this.getCount() }</span>
-        <button 
-          style={ this.styles.button } 
-          onClick={ () => { this.props.onIncrement(this.props.counter) }}
-        >+</button>
-
-        <button 
-          style={ this.styles.button } 
-          onClick={ () => { this.props.onDecrement(this.props.counter) } }
-        >-</button>
-
-        <button onClick={ () => 
-          this.props.onDelete(this.props.counter.id) } 
-          className='btn btn-danger btn-sm m-2'
-        >Delete</button>
-      </div>
-    );
-  }
-
-  getCount() {
-    const { value } = this.props.counter;
-    return (value === 0 ? 'Zero' : value);
-  }
-
-  getBadgeClasses() {
-    let spanClasses = 'badge m-2 badge-';
-    spanClasses += this.props.counter.value === 0 ? 'warning' : 'primary';
-    return spanClasses;
+    return <h1>Hello World, again</h1>;
   }
 }
  
