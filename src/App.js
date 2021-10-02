0bc49d054ee8db9cc6f53be827f72bb01f4c6154
whitewolf8383 <kandt.vandf@gmail.com>
2021-10-02 15:51:16 -0600

    A single counter completed

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Counter from './components/counter';
 class App extends React.Component {
   render() { 
     return (
-      <div>
+      <React.Fragment>
         <Counter />
-      </div>
+      </React.Fragment>
     );
   }
 }
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
 
 class Counter extends React.Component {
+  state = {
+    count: 0
+  };
+
+  formatCount() {
+    const count = this.state.count;
+    return count === 0 ? <h6>Zero</h6> : <h6>{ count }</h6>;
+  }
+
+  getClasses() {
+    let classes = "badge m-2 badge-";
+    classes += (this.state.count === 0) ? "warning" : "primary";
+    return classes;
+  }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  handleDecrement = () => {
+    const count = this.state.count;
+    this.setState({ count: count === 0 ? count : count - 1 });
+  }
+
+
   render() { 
-    return <h1>Hello World, again</h1>;
+    const buttonClasses = "btn btn-secondary btn-sm m-2";
+
+    return (
+      <div>
+        <span className={ this.getClasses() }>{ this.formatCount() }</span>
+        <button onClick={ this.handleIncrement } className={ buttonClasses }>Increment</button>
+        <button onClick={ this.handleDecrement } className={ buttonClasses }>Decrement</button>
+      </div>
+    );
   }
 }
  
